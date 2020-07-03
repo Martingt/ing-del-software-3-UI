d7f73a7236bd10eba652ba6a1d1cbd814b3ab6d9
Martin Pordomingo <martinpordomingo@gmail.com>
2020-07-03 00:56:08 -0300

    chronometer added

--- src/chronos.config.js
+++ src/chronos.config.js
@@ -1,6 +1,6 @@
 
 const Config = {
-  currentProfile: "cloud",
+  currentProfile: "localDev",
   localDev: {
     backendUrl:"http://127.0.0.1:8000/",
     resources:"http://127.0.0.1:3000/resources/",
--- src/scenes/TaskView.js
+++ src/scenes/TaskView.js
@@ -5,7 +5,7 @@ import '../resources/styles/taskView.css';
 import '../resources/styles/chronometre.css';
 import back from '../resources/images/back.png';
 import backLight from '../resources/images/back-light.png';
-import {Button} from 'reactstrap';
+import {Button, Col} from 'reactstrap';
 const currentProfile = Config.currentProfile;
 
 export default class TaskView extends Component {
@@ -20,7 +20,14 @@ export default class TaskView extends Component {
         timeIntervals: '',
         backImg: backLight,
         backActive: false,
-        tareaComenzada: true
+        tareaComenzada: false,
+        chronometreActive: false,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        totalSeconds: 0,
+        timerPaused:false,
+        tareaTerminada: false
     }
   }
 
@@ -45,25 +52,124 @@ export default class TaskView extends Component {
     }
   }
 
+  endTask = () => {
+    this.setState({chronometreActive:false, tareaTerminada:true});
+    clearInterval(this.timer);
+  }
+
+  pauseTimer =() => {
+    if(this.state.chronometreActive){
+      clearInterval(this.timer);
+      this.setState({timerPaused: true});
+    }
+  }
+
+  playTimer = () =>{
+    if(this.state.chronometreActive){
+      this.activateTimer();
+      this.setState({timerPaused: false});
+    }
+  }
+
+  startTask = () =>{
+    this.activateTimer();
+  }
+
+  activateTimer = () =>{
+    var hours =0;
+    var minutes = 0;
+    var totalSeconds = 0;
+    this.setState({chronometreActive:true, tareaComenzada:true});
+    this.timer = setInterval(()=>{
+      totalSeconds = this.state.totalSeconds+1;
+      hours = Math.floor(totalSeconds / 3600);
+      minutes = Math.floor(totalSeconds / 60);
+
+      if(this.state.seconds === 59)
+        this.setState({seconds: 0});
+      else
+        this.setState({seconds: this.state.seconds+1});
+
+      this.setState({totalSeconds: totalSeconds,
+        hours:hours, minutes:minutes});
+
+    },1000);
+  }
+
+  displayChronometer = (taskConrtrols) =>{
+
+    let taskControls = null;
+    if (this.state.tareaComenzada && this.state.tareaTerminada){
+      taskControls = (<span>Tarea finalizada con exito. </span>);
+    }
+    else if (this.state.tareaComenzada && !this.state.tareaTerminada){
+      taskControls = (<Button className="cButton"
+        onClick={this.endTask}
+        style={{minWidth:'124px'}}> Finalizar Tarea</Button>)
+    }
+    else {
+      taskControls = (<Button className="cButton"
+        onClick={this.startTask} color="success">Comenzar Tarea</Button>)
+    }
+
+    return (<div className='chronometre'>
+
+      <Col className='timer'>
+        <h3 className='tnumber'>{
+          (this.state.hours < 10)?
+          '0' + this.state.hours :
+          this.state.hours }:</h3>
+        <h3 className='tnumber'>{
+          (this.state.minutes<10)?
+          '0' + this.state.minutes :
+          this.state.minutes}:</h3>
+        <h3 className='tnumber'>{
+          (this.state.seconds < 10)?
+          '0' + this.state.seconds :
+          this.state.seconds
+         }</h3>
+      </Col>
+
+
+      <Col className='options'>
+      { (this.state.timerPaused)?
+            (<span
+              className={(this.state.chronometreActive)? "cButton" : "disabledButton"}
+              onClick={this.playTimer}>Continuar</span>):
+            (<span
+              className={(this.state.chronometreActive)? "cButton" : "disabledButton"}
+              onClick={this.pauseTimer}>Pausar</span>)}
+      {taskControls}
+      </Col>
+      </div>)
+  }
+
   render(){
+
     return (<div>
       <div className="titleContent">
-        <div className="backButton"
-        onMouseEnter={this.toggleBackImg}
-        onMouseLeave={this.toggleBackImg}
-        onClick={this.props.onBackRequest()} ><img
-        src={this.state.backImg}
-        height={14}  /></div>
-        <h4 className='title'>{this.state.title}</h4>
+          <div className="backButton"
+          onMouseEnter={this.toggleBackImg}
+          onMouseLeave={this.toggleBackImg}
+          onClick={this.props.onBackRequest()} ><img
+          src={this.state.backImg}
+          height={14}  /></div>
+          <h4 className='title'>{this.state.title}</h4>
       </div>
       <div className='division' />
-      <div className='chronometre'>
-        <div className='timer'>
-        </div>
-        <Button>Pausar Tiempo </Button>
-        {(this.state.tareaComenzada)?
-        (<Button> Finalizar Tarea</Button>):
-        (<Button>Comenzar Tarea</Button>)}
+      <span>cronometro</span>
+        {this.displayChronometer()}
+      <div>
+        <span>State:</span>
+        <h5>Descripcion</h5>
+        <span>{this.state.description}></span>
+
+        State: {this.state.state}
+        Total time: {this.state.totalSeconds}
+        <h5>Tiempo</h5>
+        <span>Tiempo trabajado total {this.state.totalTime}</span>
+        <span>Tiempo de trabajo total {this.state.workingTime}</span>
+
       </div>
     </div>);
   }
