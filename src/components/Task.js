29e5b6ced9b589e5ceed0cd36c780133ce0b7032
Martín García Tejeda <m.garcia.tejeda@gmail.com>
2020-07-24 22:00:20 -0300

    Se puede editar tareas

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import {
-  Card, CardText, CardBody
+  Card, CardText, CardBody, FormGroup, Input, Button
 } from 'reactstrap';
 import { Link } from "react-router-dom";
 import  '../resources/styles/task.css';
 import taskStyle from '../resources/styles/tasks.js';
 import clock from '../resources/images/clock.png';
 import edit from '../resources/images/edit.png';
+import CloseButton from './elements/CloseButton.js';
+import axios from 'axios';
+import Config from '../chronos.config.js';
+const currentProfile = Config.currentProfile;
+
 class Task extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      totalSeconds: props.totalTime
+      totalSeconds: props.totalTime,
+      taskEditionTried: false,
+      taskForm: {
+        title: props.title,
+        description: props.description,
+      }
     }
   }
 
@@ -40,6 +50,46 @@ class Task extends Component {
     }, 1000);
   }
 
+  handleTitleChange = (event) =>{
+    this.setState({taskForm: {...this.state.taskForm, title: event.target.value }});
+  }
+
+  handleDescriptionChange = (event) =>{
+    this.setState({taskForm: {...this.state.taskForm, description: event.target.value }});
+  }
+
+  toggleTaskEdition = () =>{
+    this.setState({taskEditionTried: !this.state.taskEditionTried});
+  }
+
+  cancelTaskCreation = () => {
+    this.setState({taskForm:{title: "", description:""}, taskEditionTried: false});
+    this.toggleTaskEdition();
+  }
+
+  editTask = () => {
+    let baseUrl = Config[currentProfile].backendUrl+'tasks/'+this.props.code;
+    axios.put(baseUrl, this.state.taskForm).then((response) => {
+        console.log(response.status);
+        this.props.title = this.state.taskForm.title;
+        this.props.description = this.state.taskForm.description;
+        this.setState({
+          taskEditionTried: false
+        })
+      }).catch(e => {
+        let errorEntries = Object.entries(e.response.data.errors)
+        let i = 0;
+        let resultsList = <div>{
+            errorEntries.map((error) => {i++;
+              return <span key={i}>{error[1]}</span>})
+          }</div>;
+  
+        this.setState({
+          taskEditionTried: false
+        })
+    });
+  }
+
   render(){
 
     let color = this.props.state === 'To Do'? "#2095ff":
@@ -52,10 +102,47 @@ class Task extends Component {
     let minutes = Math.floor((this.state.totalSeconds - hours*3600) / 60);
     let seconds = Math.floor(this.state.totalSeconds % 60);
 
-    return(
-      <Link to={this.props.onClickGoTo} className="noStyleLink" >
+    let card = null;
+    if(this.state.taskEditionTried) {
+      card = (
+        <Card className="task taskCreationCard" style={{...taskStyle.taskCard, cursor:'default'}}>
+        <FormGroup style={{display:'flex',
+        flex:1, justifyContent:'space-between', padding:10, flexDirection:'column',paddingBottom:3}}>
+
+         <div className="taskCreationBody">
+           <div className="taskCreationFormTop" style={{...taskStyle.taskCreationFormTop}}>
+             <span style={{...taskStyle.taskCreationFormTitle}}></span>
+             <CloseButton onClick={this.cancelTaskCreation}/>
+           </div>
+           <Input type="text"
+             onChange={(title)=>{this.handleTitleChange(title)}}
+             defaultValue={this.props.title}
+             name="title"
+             id="taskTitle"
+             placeholder="Nombre de la tarea"
+             style={{fontFamily:'AvenirNext-Regular', border: 'none', borderRadius:0, height:26}}/>
+
+           <Input type="textarea"
+             onChange={(desc)=>{this.handleDescriptionChange(desc)}}
+             defaultValue={this.props.description}
+             name="description"
+             id="taskTitle"
+             placeholder="Descripcion"
+             style={{marginTop:10, fontFamily:'AvenirNext-Regular',border: 'none', resize:'none', height:'100%'}} />
+         </div>
+
+         <div  style={{ display:'flex', justifyContent:'center'}}>
+           <Button size='sm' onClick={this.editTask} style={{...taskStyle.buttonCreate}}> EDITAR </Button>
+         </div>
+       </FormGroup>
+        </Card>
+      );
+    }
+    else {
+      card = (
         <Card className={cardStyle} style={{...taskStyle.taskCard}}  >
           <CardBody style={{flex:1, display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
+            <Link to={this.props.onClickGoTo} className="noStyleLink" >
               <div style={{flex:1, display:'flex', flexDirection:'column'}}>
                 <div style={{ paddingTop:5, paddingBottom:10 }}>
                   <CardText style={{fontSize:'1.1rem',fontFamily:'AvenirNext-Regular'}}>{
@@ -95,12 +182,14 @@ class Task extends Component {
                   </CardText>
                 </div>
             </div>
-            <div style={{flex:1, display:'flex', justifyContent:'flex-end', maxHeight:'20px'}}>
+            </Link>
+            <div onClick={this.toggleTaskEdition} style={{flex:1, display:'flex', justifyContent:'flex-end', maxHeight:'20px'}}>
               <img alt="edit" src={edit} height={19} width={19} style={{opacity:0.42}}></img>
             </div>
           </CardBody>
-          </Card>
-        </Link>)
+          </Card>);
+    }
+    return card;
   }
 }
 
